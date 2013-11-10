84ee36a68eafe581d206586cef89140b0e80a01d
Wade <asusHD@ASUS>
2013-11-10 12:32:37 -0500

    Not much of a commit, just had the overworld CM still on my computer.

---
+++ public_html/GameCode/CM/OverWorldCM.js
@@ -0,0 +1,11 @@
+var OverWorldCM = function(engine) {
+    var entityCM = [new Plantet1(engine), new Planet2(engine), new Planet3(engine)];
+    this.draw = function() {
+        
+    }
+    
+    this.update = function() {
+        
+    }
+}
+
