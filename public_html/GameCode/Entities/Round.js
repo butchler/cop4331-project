d0d7679cd848f911c63050c695b4f042e5b7f25e
rick <rick@rick-PC>
2013-11-23 19:42:31 -0500

    preping enemies

--- public_html/GameCode/Entities/Round.js
+++ public_html/GameCode/Entities/Round.js
@@ -2,6 +2,18 @@ var Round = function (engine) {
     var waves = [];
     var wave = 0;
     
+    
+    var activeWaves = 0;
+    
+    var spinners = [];
+    var spinnerCount = Math.floor(globals.level.difficulty / 2);
+    
+    var seekers = [];
+    var seekerCount = Math.floor(globals.level.difficulty / 2);
+    
+    var boss = [];
+    var bossCount = Math.floor(globals.level.difficulty / 10);
+    
     var nextWave = (20 - globals.level.difficulty) * 60;
     var nextWaveCounter = nextWave;
     
@@ -18,17 +30,27 @@ var Round = function (engine) {
                 waves[i].draw();
     }
     
+    
     this.update = function (bullets) {
-        if (wave < waves.length && ++nextWaveCounter >= nextWave) {
+        
+        // spawn logic for a new wave, don't create a wave if we're out waves
+        // don't create a wave before a certain amount of time has passed
+        // don't create a wave if we already have 2 active waves
+        if (wave < waves.length 
+                && ++nextWaveCounter >= nextWave 
+                && activeWaves <= 2) {
+            
             nextWaveCounter = 0;
             wave++;
         }
         
         
+        // update a wave if the wave still exists
         for (var i = 0; i < wave; i++) {
             if (waves[i] !== undefined) {
                 waves[i].update(bullets);
                 
+                // if wave is dead, turn empty it and set it to undefined
                 if (!waves[i].isAlive()) {
                     waves[i].empty();
                     waves[i] = undefined;
@@ -36,11 +58,20 @@ var Round = function (engine) {
             }
         }
         
+        // reset the number of active waves
+        activeWaves = 0;
         var isAlive = 0;
         
-        for (var i = 0; i < waves.length; i++)
-            if (waves[i] !== undefined && waves[i].isAlive()) isAlive++;
+        // check how many waves are still alive, and how many are active
+        for (var i = 0; i < waves.length; i++) {
+            if (waves[i] !== undefined && waves[i].isAlive()) {
+                isAlive++;
+                activeWaves++;
+            }
+        }
         
+        
+        // if everything is dead, win game
         if (isAlive == 0) {
             winProcedure();
         }
